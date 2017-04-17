ff34c3e1132f3212791fbb09835fd572dd2149d7
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-04-18 00:24:38 +0530

    string to html converson done using react-parser

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './home/Home';
 import CreateNote from './note/CreateNote';
+import ViewNote from './note/ViewNote';
 
 const App = () => (
   <Router> 
     <div>
     	<Route exact path="/" component={Home}></Route>
     	<Route path="/create" component={CreateNote}></Route>
+    	<Route path="/view" component={ViewNote}></Route>
   	</div>
   </Router>
 )
