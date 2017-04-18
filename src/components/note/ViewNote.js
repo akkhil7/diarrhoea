d412a254e97261f05c246a3bbe914ad3ee993c74
Akhil Ramesh <akkhil@outlook.com>
2017-04-18 23:48:34 +0530

    small fix

--- src/components/note/ViewNote.js
+++ src/components/note/ViewNote.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactHtmlParser from 'react-html-parser';
+import Parser from 'react-html-parser';
 
 class ViewNote extends React.Component{
 	render(){
 		var html = "<p>hellooo my name is mrinal how are you?</p>"
 		return(
-			<div>{ ReactHtmlParser(html) }</div>
+			<div>{ Parser(html) }</div>
 		)
 	}
 
