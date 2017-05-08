86f9c375a01212fb04804ece432218e7fcf47f2b
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-05-08 23:36:41 +0530

    API added.Notes added

--- src/components/note/CreateNote.js
+++ src/components/note/CreateNote.js
@@ -2,6 +2,7 @@ import React from 'react';
 import RichTextEditor from 'react-rte';
 import { connect } from 'react-redux';
 import { verifyCurrentUser } from '../../actions/userActions';
+import API from '../API';
 
 const mapStateToProps = (store) => {
   return {
@@ -23,6 +24,23 @@ class CreateNote extends React.Component{
    		this.setState({value});
   	};
 	
+	handleSubmit(){
+		var entry = this.state.value.toString('html');
+		var id = this.props.currentUser.id;
+		var url = API.url('notes');
+		var note={
+			entry: entry,
+			id: id
+		}
+        var success = (res) => {
+            console.log("New note added successfully");
+        }
+        var failure = (res) => {
+            console.log("Failed to add new note");
+        }
+        API.post(url,note,success,failure)
+	}
+
 	render(){
 		var entry = this.state.value.toString('html');
 		return(
@@ -33,6 +51,7 @@ class CreateNote extends React.Component{
           		value={this.state.value}
           		onChange={this.onChange.bind(this)}
         		/>
+        		<button onClick={this.handleSubmit.bind(this)}>SUBMIT</button>
 				</div>
 			</div>
 		)
