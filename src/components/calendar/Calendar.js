3d540793dbed6f3240e7f5ca5629ce3c774a1ea8
Akhil Ramesh <akkhil@outlook.com>
2017-06-27 00:33:04 +0530

    calendar modified and working + display preview working

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,21 +3,15 @@ import React from 'react';
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 import API from '../API.js';
-const birthdays = {
-  3: [{ name: 'Mirko', age: 35 }, { name: 'Gianluca', age: 29 }],
-  8: [{ name: 'Elena', age: 21 }],
-  9: [{ name: 'Irene', age: 43 }],
-  12: [{ name: 'Paolo', age: 78 }, { name: 'Giorgia', age: 18 }],
-  18: [{ name: 'Claudia', age: 54 }],
-  22: [{ name: 'Maria', age: 9 }, { name: 'Luigi', age: 67 }],
-  25: [{ name: 'Simone', age: 31 }],
-  26: [{ name: 'Marta', age: 46 }],
-};
+import _ from 'lodash';
+
 class Calendar extends React.Component {
   constructor() {
     super()
     this.state = {
       notes: [],
+      selectedNote: {},
+      flag: false
     }
   }
 
@@ -28,6 +22,7 @@ class Calendar extends React.Component {
       console.log(res)
       this.setState({notes:res})
       this.toggleDayBox();
+      this.modifyCalendarStyle();
       
     }
 
@@ -40,6 +35,14 @@ class Calendar extends React.Component {
 
   }
 
+  modifyCalendarStyle() {
+    const nodes = document.getElementsByClassName('DayPicker-Day--highlight')
+    console.log(nodes)
+    for(var i=0;i<nodes.length;i++) {
+      nodes[i].firstChild.style.backgroundColor = '#00a2ff';
+      nodes[i].firstChild.style.boxShadow = '0 0 3px #00a2ff';
+    }
+  }
   toggleDayBox() { //getting each day of the month from calendar
     const notes = this.state.notes
     console.log(notes)
@@ -54,6 +57,36 @@ class Calendar extends React.Component {
 
   }
 
+  handleDayClick(day,modifiers,event) {
+
+    console.log(event)
+    const node = event.target
+    const notes = this.state.notes
+    const dayDate = new Date(day)
+    dayDate.setHours(0,0,0);
+   // console.log(dayDate)
+    var flag = false;
+    for(var i=0;i<notes.length;i++) {
+      const noteDate = new Date(notes[i].created_at)
+      noteDate.setHours(0,0,0,0);
+      console.log(dayDate)
+      console.log(noteDate)
+      if (+dayDate === +noteDate)
+        {
+          const note = notes[i]
+          this.setState({
+            selectedNote: note,
+          })
+          break;
+        }
+    } // found note corresponding to date
+
+  }
+
+  handlePreviewClose() {
+    this.setState({selectedNote: {}});
+  }
+  
   render() {
     console.log(this.state.days)
     const modifiers = {
@@ -63,19 +96,35 @@ class Calendar extends React.Component {
     const modifiersStyles = {
       highlight: {
         color: '#ffffff',
-        backgroundColor: '#4ab8f7',
-        cursor: 'pointer'
+        cursor: 'pointer',
       }
     }
+    var displayPreview = ""
+    const selectedNote = this.state.selectedNote
+    if(!_.isEmpty(this.state.selectedNote))
+    {
+      displayPreview = (<div className="display-preview">
+                          <img src="close.svg" className="close" onClick={this.handlePreviewClose.bind(this)} />
+                          <h2>{selectedNote.entry} - My name is Akhil and how I bought a Lamborghini at the age of 23!</h2>
+                        </div>)
+    }
+
 
     console.log(modifiers)
     return(
       <div className="calendar-container">
-      <DayPicker 
-      onDayClick={day => alert(day)}
-      modifiers={modifiers}
-      modifiersStyles = {modifiersStyles}
-      />
+        <div className="calendar-component">
+        <DayPicker 
+        onDayClick={this.handleDayClick.bind(this)}
+        modifiers={modifiers}
+        modifiersStyles = {modifiersStyles}
+        />
+        </div>
+        <div className="recent-posts">
+        <h2> Recent Posts </h2>
+        <hr />
+        </div>
+        {displayPreview}        
       </div>
 
     )
