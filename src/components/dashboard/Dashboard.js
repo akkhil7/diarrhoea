20231f0011ad6a82126c2dde39c58c63166bcbe1
mrinalkrishnanm <mrinal.krishnan06@gmail.com>
2017-06-29 00:43:21 +0530

    dashboard redux added

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import Calendar from '../calendar/Calendar.js';
 import Navbar from '../navbar/Navbar.js';
+import { loadCalendarData } from '../../actions/dashBoardActions';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (store) => {
+  return {
+   note: store.dashboard.note
+  }
+}
 
 class Dashboard extends React.Component{
+	componentDidMount(){
+		this.props.dispatch(loadCalendarData())
+	}
 	render(){
 		return(
       <div className="dashboard-container">
@@ -12,5 +23,7 @@ class Dashboard extends React.Component{
 			)
 	}
 }
+
+Dashboard = connect(mapStateToProps)(Dashboard);
 
 module.exports = Dashboard;
